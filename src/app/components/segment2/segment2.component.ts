941ad629b33945d0c1b8cb3c505ea725f6dc0c7c
Mike Langendorf <mike.langendorf@gmail.com>
2019-02-20 18:48:47 +0100

    initial commit

---
+++ src/app/components/segment2/segment2.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-segment2',
+  templateUrl: './segment2.component.html',
+  styleUrls: ['./segment2.component.scss'],
+})
+export class Segment2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/tab1/tab1.module.ts
+++ src/app/tab1/tab1.module.ts
@@ -1,17 +1,52 @@
 import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab1Page } from './tab1.page';
+import { Segment1Component } from '../components/segment1/segment1.component';
+import { Segment2Component } from '../components/segment2/segment2.component';
+import { Segment3Component } from '../components/segment3/segment3.component';
+import { Segment1Module } from '../components/segment1/segment1.module';
+import { Segment2Module } from '../components/segment2/segment2.module';
+import { Segment3Module } from '../components/segment3/segment3.module';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: Tab1Page,
+    children: [
+      {
+        path: '',
+        redirectTo: 'segment1'
+      },
+      {
+        path: 'segment1',
+        component: Segment1Component,
+      },
+      {
+        path: 'segment2',
+        component: Segment2Component,
+      },
+      {
+        path: 'segment3',
+        component: Segment3Component
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    RouterModule.forChild([{ path: '', component: Tab1Page }])
+    RouterModule.forChild(routes),
+    Segment1Module,
+    Segment2Module,
+    Segment3Module
   ],
   declarations: [Tab1Page]
 })
-export class Tab1PageModule {}
+export class Tab1PageModule {
+}
